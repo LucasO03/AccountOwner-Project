ab8109a6fff0df2c0e8abe95dbdc2411c2f1e905
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2023-04-11 19:52:07 -0300

    Mudado a rota e corrigido erro de digitação

--- backend/Entities/DataTranferObjects/OwnerDto.cs
+++ backend/Entities/DataTranferObjects/OwnerDto.cs
@@ -5,7 +5,7 @@ public class OwnerDto
     public Guid Id { get; set; }
     public string Name { get; set; }
     public DateTime DateOfBirth { get; set; }
-    public string Adress { get; set; }
+    public string Address { get; set; }
 
     public IEnumerable<AccountDto> Accounts { get; set; }
 }
