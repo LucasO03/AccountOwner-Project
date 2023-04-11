4dcd3b746055de96515c875af966add1aab334e0
Lucas Santos de Oliveira <lucas.santos003@outlook.com>
2023-04-11 20:19:47 -0300

    Criado e feito a rota da pagina de detalhes dos owners

--- frontend/AccountOwnerClient/src/app/app.module.ts
+++ frontend/AccountOwnerClient/src/app/app.module.ts
@@ -9,14 +9,16 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
-import { OwnerModule } from './owner/owner.module'
+import { OwnerModule } from './owner/owner.module';
+import { InternalServerComponent } from './error-pages/internal-server/internal-server.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    InternalServerComponent
   ],
   imports: [
     BrowserModule,
